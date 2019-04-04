a1e296eefac7d053e0b09d0bf0b380c1341f49b0
Marcos Lopez <43101560+Miercos@users.noreply.github.com>
2019-04-03 17:53:45 -0700

    Add files via upload

--- ForniteAPI/src/Main.py
+++ ForniteAPI/src/Main.py
@@ -1,1 +1,12 @@
+from ray import Reader
+import sys
 
+#print(sys.argv[1])
+
+with Reader("C://Users/Marcos/AppData/Local/FortniteGame/Saved/Demos" + "/" + sys.argv[1]) as replay:
+    print(replay.stats['eliminations'])
+    print(replay.stats['assists'])
+    print(replay.stats['revives'])
+    print(replay.stats['accuracy'])
+    print(replay.stats['damage_taken'])
+    print(replay.team_stats['position'])
