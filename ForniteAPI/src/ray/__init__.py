23a7f2d4711106c0850fd109ebadbb43ba29553c
Eric Liang <liange3@uci.edu>
2019-04-18 10:15:27 -0700

    Updated with database problems

---
+++ ForniteAPI/src/ray/__init__.py
@@ -0,0 +1,10 @@
+"""
+Fortnite replay parser
+"""
+
+__version__ = '0.1.7'
+
+from .logging import logger
+from .models import *
+from .exceptions import *
+from .reader import Reader
---
+++ ForniteAPI/src/ray/exceptions.py
@@ -0,0 +1,11 @@
+class InvalidReplayException(Exception):
+    pass
+
+class ReadStringException(Exception):
+    pass
+
+class FrameParsingException(Exception):
+    pass
+
+class PlayerEliminationException(Exception):
+    pass
