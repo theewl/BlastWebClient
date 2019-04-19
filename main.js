c794c8fa2d2f326f40c5ad00ca2006556b5196ec
Marcos Lopez <43101560+Miercos@users.noreply.github.com>
2019-04-19 13:20:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ let win
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({ width: 900, height: 600, minWidth:900, icon:"./images/icon.ico"})
+    
+    // Remove  menu
+    win.setMenu(null)
 
     // and load the index.html of the app.
     win.loadFile('index.html')
