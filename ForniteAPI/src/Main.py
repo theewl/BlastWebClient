83ab7fe24df713c26402cf8828e95bf6aa2a512e
Eric Liang <liange3@uci.edu>
2019-05-29 16:05:11 -0700

    Added updated paths and some small stuff to csgo

--- ForniteAPI/src/Main.py
+++ ForniteAPI/src/Main.py
@@ -1,10 +1,19 @@
 from ray import Reader
 import sys
+import os
+path = os.getcwd()
 
-#print(sys.argv[1])
+dashChecker = 0
+
+for x in range(len(path)):
+    if path[x] == '/':
+        dashChecker += 1
+    
+    if path[x] == '/' and dashChecker == 3:
+        newPath = path[0:x+1]
 
 # ideally we will pass the entire path via parameter
-with Reader("/Users/kimki/Desktop/BlastWebClient/Replays" + "/" + sys.argv[1]) as replay:
+with Reader(newPath + "AppData/Local/FortniteGame/Saved/Demos" + "/" + sys.argv[1]) as replay:
     print(replay.stats['eliminations'])
     print(replay.stats['assists'])
     print(replay.stats['revives'])
