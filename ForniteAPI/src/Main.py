3cb87b6eca81abf4115c8f6b1f0c0313d96163ab
Alex Tran <altran2@uci.edu>
2019-05-29 21:03:40 -0700

    Initial commt

--- ForniteAPI/src/Main.py
+++ ForniteAPI/src/Main.py
@@ -6,14 +6,14 @@ path = os.getcwd()
 dashChecker = 0
 
 for x in range(len(path)):
-    if path[x] == '/':
+    if path[x] == '\\':
         dashChecker += 1
-    
-    if path[x] == '/' and dashChecker == 3:
+
+    if path[x] == '\\' and dashChecker == 3:
         newPath = path[0:x+1]
 
 # ideally we will pass the entire path via parameter
-with Reader(newPath + "AppData/Local/FortniteGame/Saved/Demos" + "/" + sys.argv[1]) as replay:
+with Reader(newPath + "AppData\\Local\\FortniteGame\\Saved\\Demos" + "\\" + sys.argv[1]) as replay:
     print(replay.stats['eliminations'])
     print(replay.stats['assists'])
     print(replay.stats['revives'])
